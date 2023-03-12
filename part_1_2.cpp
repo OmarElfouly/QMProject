e9ca8f5dfc569adae1c96f8b350c38561a2ebcd9
Ahmed-A03 <ahmed-a@aucegypt.edu>
2023-03-12 20:41:47 +0200

    Added my implementation theory.

--- part_1_2.cpp
+++ part_1_2.cpp
@@ -3,6 +3,17 @@
 #include <string>
 #include <stack>
 
+/*
+A: Part 1 and 2, Ahmed Ali
+
+- Implementation theory: Use a stack to find every matching pair of brackets. For each pair, determine whether it contains purely a product,
+or if it contains a sum. Once each pair is determined, loop through the input, and whenever we reach a "warning" input (a multiplication followed by something unknown)
+then we check that there is no violation (sum * product, multi-var NOT). If we find a "+", we are fine, if not, we must check. I will use a vector of a pair of a pair of integers
+and a character, and the pair of inetegrs is the indices of the opening and closing bracket, and the character tells me if it's a sum, 's' or a product, 'p'.
+I will also need to have a checker before checking whether a bracket contains a sum or a product, which checks that if the character after a bracket is a not, "'", then
+I need to make sure that the bracket does not contain more than one variable, for example, (abc)'.
+*/
+
 using namespace std;
 
 vector<string> split(string input, string delim)		//string splitting function, splits function between each sum (delimiter), each product is an element.
@@ -36,4 +47,6 @@ bool validation(string input, vector<string> terms)
 
 	if(!(arr[0] == arr[1]))		//if the number of opening and closing brackets are unequal, then the input is invalid.
 		return false;
+
+
 }
