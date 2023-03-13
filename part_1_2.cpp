4f753c7d8b035d57bb2be68ec33a462af51c6cd7
Ahmed-A <ahmed-a@aucegypt.edu>
2023-03-13 17:00:42 +0200

    added a bracket pairing function.

--- part_1_2.cpp
+++ part_1_2.cpp
@@ -24,8 +24,31 @@ vector<string> split(string input, string delim)		//string splitting function, s
 	return terms;
 }
 
+vector<pair<int, int>> bracketPairs(string input)		//function to find each pair of brackets
+{
+	vector<pair<int, int>> pairs;
+	stack<int> stack;
+
+	for (int i = 0; i < input.size(); i++) {		//loop through the input string, and once an opening bracket is found, it's index is pushed into the stack
+		if (input[i] == '(') {						//we keep doing this until a closing bracket is found. This closing bracket must then be the
+			stack.push(i);							//corresponding bracket to the opening bracket in the top of the of the stack.
+		}											
+		else if (input[i] == ')') {
+			if (!stack.empty()) {						//if we have an open bracket in the stack
+				int openBracket = stack.top();			//then we extract it's index
+				stack.pop();
+				pairs.push_back(make_pair(openBracket, i));	//and make a pair of the open bracket and the closed bracket, and push this into our vector of pairs of brackets
+			}
+		}
+	}
+
+	return pairs;
+}
+
 bool validation(string input, vector<string> terms)
 {
+	bool defaultBool = false;
+
 	input.erase(remove(input.begin(), input.end(), ' '), input.end());		//remove any spaces in the input, easier to handle.
 	transform(input.begin(), input.end(), input.begin(), ::tolower);		//all characters are lowercase for convencience and consistency.
 	if (((input[0] < 97 || input[0] > 122) && input[0] != 40) || ((input.back() < 97 || input.back() > 122) && input.back() != 41 && input.back() != 39))
@@ -46,5 +69,5 @@ bool validation(string input, vector<string> terms)
 	else if (arr[0] == 0 && arr[1] == 0)
 		return true;				//if there are no brackets, then it is in SoP form.
 
-
+	return defaultBool;
 }
